2995dff264f627ca6bc1b4b21a688ecead63e43a
Craig Crosby <creecros@gmail.com>
2018-11-28 09:40:10 -0500

    Create Plugin.php

---
+++ Plugin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kanboard\Plugin\RemoveSwimlaneDetails;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+
+class Plugin extends Base
+{
+  public function initialize()
+  {
+    $this->template->setTemplateOverride('board/table_column', 'removeswimlanedetails:board/table_column');
+
+  }
+  public function getPluginName()
+  {
+    return 'RemoveSwimlaneDetails';
+  }
+  public function getPluginAuthor()
+  {
+    return 'Craig Crosby';
+  }
+  public function getPluginVersion()
+  {
+    return '1.0.0';
+  }
+  
+  public function getPluginDescription()
+  {
+    return 'Removes the total swimlane task count if only 1 swimlane is used';
+  }
+  public function getPluginHomepage()
+  {
+    return 'https://github.com/creecros/oneswimlanetorulethemall';
+  }
+}
